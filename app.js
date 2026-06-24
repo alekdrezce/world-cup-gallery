06e31fbe02f56996af870afc94b99f540f1b7da8
AlekDrezce <alekdz74@gmail.com>
2026-06-23 22:44:22 -0300

    13

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const mundiales = {
     },
     "1978": { 
         pais: "Argentina", 
-        marca: "-", 
+        marca: "Adidas", 
         detalles: "Camiseta de piqué técnico, bastones celestes/blancos y cuello redondo." 
     },
     "1982": { 
@@ -62,52 +62,52 @@ const mundiales = {
     },
     "1986": { 
         pais: "Argentina", 
-        marca: "-", 
+        marca: "Le Coq Sportif", 
         detalles: "Camiseta de poliester microperforado, cuello redondo y bastones." 
     },
     "1990": { 
         pais: "Alemania", 
-        marca: "-", 
+        marca: "Adidas", 
         detalles: "Camiseta de poliéster brillante blanco, cuello polo y gráfico geométrico." 
     },
     "1994": { 
         pais: "Brasil", 
-        marca: "-", 
+        marca: "Umbro", 
         detalles: "Camiseta de poliéster Jacquard amarillo, cuello polo y escudos en marca de agua." 
     },
     "1998": { 
         pais: "Francia", 
-        marca: "-", 
+        marca: "Adidas", 
         detalles: "Camiseta de tejido técnico satinado azul Francia con cuello polo blanco, complementada con paneles de ventilación discretos en los costados." 
     },
     "2002": { 
         pais: "Brasil", 
-        marca: "-", 
+        marca: "Nike", 
         detalles: "Camiseta de tejido de alto rendimiento amarillo vivo con paneles ergonómicos verdes, ventilación dinámica en los costados y doble capa interna transpirable." 
     },
     "2006": { 
         pais: "Italia", 
-        marca: "-", 
+        marca: "Puma", 
         detalles: "Camiseta de licra elástica de ajuste ceñido al cuerpo azul azzurra con sutiles paneles de ventilación simétricos en color negro bajo las axilas." 
     },
     "2010": { 
-        pais: "Espana", 
-        marca: "-", 
+        pais: "España", 
+        marca: "Adidas", 
         detalles: "Camiseta de tejido técnico reciclado en tono azul marino, con cuello de solapas finas, detalles en contraste rojo y amarillo en los hombros, y el escudo nacional coronado por una estrella dorada bordada de emergencia para el partido definitivo." 
     },
     "2014": { 
         pais: "Alemania", 
-        marca: "-", 
+        marca: "Adidas", 
         detalles: "Camiseta ultraligera de alto rendimiento blanca, con cuello redondo y una pronunciada \"V\" en el pecho estampada en degradé rojo y negro." 
     },
     "2018": { 
         pais: "Francia", 
-        marca: "-", 
+        marca: "Nike", 
         detalles: "Camiseta de tejido técnico texturizado azul marino oscuro, con cuello de solapa abotonada que oculta la bandera nacional, un tramado gráfico en zig-zag azul claro en las mangas hiperdinámicas y un patrón interno de ventilación transpirable de última generación." 
     },
     "2022": { 
         pais: "Argentina", 
-        marca: "-", 
+        marca: "Adidas", 
         detalles: "Camiseta de tejido ultraligero termo-regulado, cuello redondo, el Sol de Mayo estampado en la nuca y el escudo coronado por detalles dorados texturizados." 
     }
 };
